8a47a7e153d656959e324ddc368380d70fed1260
ici-903-00 <ici-903-00@DESKTOP-9H8V5DQ>
2021-09-23 14:49:00 +0900

    modify.jsp 추가

--- src/main/java/com/javagyojin/ex/ModifyOk.java
+++ src/main/java/com/javagyojin/ex/ModifyOk.java
@@ -10,29 +10,27 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class JoinOk
+ * Servlet implementation class ModifyOk
  */
-@WebServlet("/JoinOk")
-public class JoinOk extends HttpServlet {
+@WebServlet("/ModifyOk")
+public class ModifyOk extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
 	
-	
 	private Connection connection;
 	private Statement stmt;
 	
 	private String name, id, pw, phone1, phone2, phone3, gender;
 	
+	HttpSession httpSession;
 	
-	
-	
-	
-    public JoinOk() {
+    public ModifyOk() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -43,7 +41,6 @@ public class JoinOk extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.println("doGet!");
 		actionDo(request, response);
 		
 	}
@@ -53,26 +50,26 @@ public class JoinOk extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		System.out.println("doPost!");
+		//doGet(request, response);
 		actionDo(request, response);
+		
 	}
-	
+
 	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
 		request.setCharacterEncoding("EUC-KR");
 		
 		name = request.getParameter("name");
-		id = request.getParameter("id");
+		//id = request.getParameter("id");
+		id = (String)httpSession.getAttribute("id");
 		pw = request.getParameter("pw");
 		phone1 = request.getParameter("phone1");
 		phone2 = request.getParameter("phone2");
 		phone3 = request.getParameter("phone3");
 		gender = request.getParameter("gender");
 		
-		//String query = "update members set name='" + name + "', phone1 = '" + phone1 + "', phone2 = '" + phone2 + "', phone3 = '" + phone3 + "', gender = '" + gender + "' where id='" + id + "'  ";
-		String query = "insert into members values('" + name + "','" + id + "', '" + pw + "', '" + phone1 + "', '" + phone2 + "', '" + phone3 + "', '" + gender + "')";
+		String query = "update members set name='" + name + "', phone1 = '" + phone1 + "', phone2 = '" + phone2 + "', phone3 = '" + phone3 + "', gender = '" + gender + "' where id='" + id + "'  ";
 		
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -82,11 +79,12 @@ public class JoinOk extends HttpServlet {
 			int i = stmt.executeUpdate(query);//query가 성공하면 정수 1을 반환
 			
 			if(i == 1) {
-				System.out.println("DB 저장 성공!!!");
-				response.sendRedirect("joinResult.jsp");//DB저장에 성공하면 joinResult.jsp로 이동
+				System.out.println("정보 수정 성공!!!");
+				httpSession.setAttribute("name", name);//수정된 이름을 세션에 overwrite
+				response.sendRedirect("modifyResult.jsp");//정보 수정에 성공하면 modifyResult.jsp로 이동
 			} else {
-				System.out.println("DB 저장 실패!!!");
-				response.sendRedirect("join.html");//DB저장에 실패하면 join.html로 다시 이동
+				System.out.println("정보 수정 실패!!!");
+				response.sendRedirect("modify.jsp");//정보 수정에 실패하면 modify.jsp로 이동하여 다시 정보 입력
 			}
 			
 		} catch (Exception e) {
@@ -103,5 +101,7 @@ public class JoinOk extends HttpServlet {
 		
 		
 	}
-
+	
+	
+	
 }
