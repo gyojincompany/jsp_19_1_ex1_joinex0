e21d54dae350d2e5317ff7609ef4af12469b951f
ici-903-00 <ici-903-00@DESKTOP-9H8V5DQ>
2021-09-23 12:06:47 +0900

    member join v1.0

---
+++ src/main/java/com/javagyojin/ex/LoginOk.java
@@ -0,0 +1,107 @@
+package com.javagyojin.ex;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginOk
+ */
+@WebServlet("/LoginOk")
+public class LoginOk extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	
+	private Connection connection;
+	private Statement stmt;
+	private ResultSet resultSet;
+	
+	private String name, id, pw, phone1, phone2, phone3, gender;
+	
+	
+    public LoginOk() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		actionDo(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		actionDo(request, response);
+	}
+	
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		request.setCharacterEncoding("EUC-KR");
+		
+		id = request.getParameter("id");
+		pw = request.getParameter("pw");
+		
+		String query = "select * from members where id = '" + id + "' and pw ='" + pw + "'";
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","scott","tiger");
+			stmt = connection.createStatement();
+			resultSet = stmt.executeQuery(query);
+			
+			while(resultSet.next()){
+				
+				String name = resultSet.getString("name");
+				String id = resultSet.getString("id");
+				String pw = resultSet.getString("pw");
+				String phone1 = resultSet.getString("phone1");
+				String phone2 = resultSet.getString("phone2");
+				String phone3 = resultSet.getString("phone3");
+				String gender = resultSet.getString("gender");
+			}
+			
+			HttpSession httpSession = request.getSession();//技记按眉 积己
+			httpSession.setAttribute("id", id);
+			httpSession.setAttribute("pw", pw);
+			httpSession.setAttribute("name", name);
+			
+			response.sendRedirect("loginResult.jsp");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(stmt != null) stmt.close();
+				if(connection != null) connection.close();
+			} catch (Exception e){
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
+	}
+
+}
